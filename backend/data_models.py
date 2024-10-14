8ebd72deddac562e26cda048ba53f90b5a69ac7d
mariacristinafadgyas <fadgyas.maria@yahoo.com>
2024-10-14 23:23:56 +0300

    updated user object to include password hash

--- backend/data_models.py
+++ backend/data_models.py
@@ -1,6 +1,7 @@
 from db_constraints import *
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.types import Enum
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()  # Creates a db object
 
@@ -12,7 +13,7 @@ class User(db.Model):
 
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(100), nullable=False, unique=True, index=True)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password = db.Column(db.String(128), nullable=False)
     role = db.Column(Enum(RoleEnum), nullable=False)
     is_active = db.Column(db.Boolean, default=True)
 
@@ -20,8 +21,18 @@ class User(db.Model):
     owner = db.relationship('Owner', backref='user', uselist=False)  # One-to-One with Owner
     customer = db.relationship('Customer', backref='user', uselist=False)  # One-to-One with Customer
 
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def save(self):
+        db.session.add(self)
+        db.commit()
+
     def __repr__(self):
-        return f"User: {self.username}, role{self.role}"
+        return f"{self.username}"
 
 
 class Owner(db.Model):
